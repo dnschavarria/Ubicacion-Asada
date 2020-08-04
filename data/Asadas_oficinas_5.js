db8019d0d515be400cb922b53e9f5f6b74d89597
dnschavarria <54127679+dnschavarria@users.noreply.github.com>
2020-08-04 07:54:53 -0600

    Add files via upload

---
+++ data/Asadas_oficinas_5.js
@@ -0,0 +1,21 @@
+var json_Asadas_oficinas_5 = {
+"type": "FeatureCollection",
+"name": "Asadas_oficinas_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "N°": 1.0, "ASADA": "Playa Potrero", "Longitud": -85.772179, "Latitud": 10.459022, "Detalles": null }, "geometry": { "type": "Point", "coordinates": [ -85.772179, 10.459022 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "N°": 2.0, "ASADA": "Surfside", "Longitud": -85.765004, "Latitud": 10.445908, "Detalles": null }, "geometry": { "type": "Point", "coordinates": [ -85.765004, 10.445908 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "N°": 3.0, "ASADA": "Brasilito", "Longitud": -85.795105, "Latitud": 10.411407, "Detalles": null }, "geometry": { "type": "Point", "coordinates": [ -85.795105, 10.411407 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "N°": 4.0, "ASADA": "Huacas", "Longitud": -85.776843, "Latitud": 10.368821, "Detalles": null }, "geometry": { "type": "Point", "coordinates": [ -85.776843, 10.368821 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "N°": 5.0, "ASADA": "El Llano", "Longitud": -85.747126, "Latitud": 10.361208, "Detalles": null }, "geometry": { "type": "Point", "coordinates": [ -85.747126, 10.361208 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "N°": 6.0, "ASADA": "La Garita", "Longitud": -85.792065, "Latitud": 10.350486, "Detalles": null }, "geometry": { "type": "Point", "coordinates": [ -85.792065, 10.350486 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "N°": 7.0, "ASADA": "Matapalo", "Longitud": -85.810342, "Latitud": 10.363043, "Detalles": null }, "geometry": { "type": "Point", "coordinates": [ -85.810342, 10.363043 ] } },
+{ "type": "Feature", "properties": { "fid": "8", "N°": 8.0, "ASADA": "Playa Grande", "Longitud": -85.84388, "Latitud": 10.34313, "Detalles": null }, "geometry": { "type": "Point", "coordinates": [ -85.84388, 10.34313 ] } },
+{ "type": "Feature", "properties": { "fid": "9", "N°": 9.0, "ASADA": "Santa Rosa", "Longitud": -85.790743, "Latitud": 10.312411, "Detalles": null }, "geometry": { "type": "Point", "coordinates": [ -85.790743, 10.312411 ] } },
+{ "type": "Feature", "properties": { "fid": "10", "N°": 10.0, "ASADA": "Villareal", "Longitud": -85.804391, "Latitud": 10.305592, "Detalles": null }, "geometry": { "type": "Point", "coordinates": [ -85.804391, 10.305592 ] } },
+{ "type": "Feature", "properties": { "fid": "11", "N°": 11.0, "ASADA": "Tamarindo", "Longitud": -85.83762, "Latitud": 10.293869, "Detalles": null }, "geometry": { "type": "Point", "coordinates": [ -85.83762, 10.293869 ] } },
+{ "type": "Feature", "properties": { "fid": "12", "N°": 12.0, "ASADA": "Lorena", "Longitud": -85.711019, "Latitud": 10.360652, "Detalles": null }, "geometry": { "type": "Point", "coordinates": [ -85.711019, 10.360652 ] } },
+{ "type": "Feature", "properties": { "fid": "13", "N°": 13.0, "ASADA": "Marvista", "Longitud": -85.783751, "Latitud": 10.417406, "Detalles": "No tiene oficina, estas coordenadas son de la escuela" }, "geometry": { "type": "Point", "coordinates": [ -85.783751, 10.417406 ] } },
+{ "type": "Feature", "properties": { "fid": "14", "N°": 14.0, "ASADA": "Lomas de Matapalo", "Longitud": -85.802449, "Latitud": 10.346146, "Detalles": "No tiene oficina, estas coordenadas son del salón comunal" }, "geometry": { "type": "Point", "coordinates": [ -85.802449, 10.346146 ] } }
+]
+}
